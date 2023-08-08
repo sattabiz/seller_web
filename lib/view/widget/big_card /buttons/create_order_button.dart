de585e726a3aaede234e112c74992ec03e1f1e12
Atakan <atakangobelek@satta.biz>
2023-08-08 11:10:03 +0300

    create order by proposal API added

--- lib/view/widget/big_card /buttons/create_order_button.dart
+++ lib/view/widget/big_card /buttons/create_order_button.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CreateOrderButton extends StatelessWidget {
+import '../../../../view_model/create_order_view_model.dart';
+import '../../../../view_model/provider_controller.dart';
+import '../../../order_view/order_view.dart';
+
+class CreateOrderButton extends ConsumerWidget {
   final String className;
+  final String? id;
   const CreateOrderButton({ 
     Key? key,
     required this.className,
+    this.id,
     }) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context, WidgetRef ref){
     return  ElevatedButton(
       style: ButtonStyle(
         backgroundColor: MaterialStatePropertyAll(Theme.of(context).colorScheme.primary),
         fixedSize: const MaterialStatePropertyAll(Size(140, 40))
       ),
-      onPressed: () {
+      onPressed: () async{
+        await ref.watch(createOrderProvider);
+        ref.read(drawerCountProvider.notifier).state = 1;
+        Navigator.pushReplacement(
+          context, 
+          PageRouteBuilder(
+            pageBuilder: (context, animation1, animation2) => OrderView(),
+            transitionDuration: const Duration(seconds: 0),
+            settings: RouteSettings(name: '/order', arguments: OrderView())
+          ), 
+        );
       }, 
       child: Text(
         FlutterI18n.translate(context, "tr.$className.create_order_btn"),
