d44c1ff71a5ff3194e52fcaa8894ce7c485e9a34
ozcpr <ozanium@outlook.com>
2023-08-18 21:15:05 +0300

    refactor proposal view

--- lib/view/widget/big_card /table/proposal_table.dart
+++ lib/view/widget/big_card /table/proposal_table.dart
@@ -9,210 +9,187 @@ class ProposalTable extends StatelessWidget {
   final List productsProposalList;
   final String className;
 
-  const ProposalTable({
-    super.key, 
-    required this.productsProposalList,
-    required this.className
-    });
-
+  const ProposalTable({super.key, required this.productsProposalList, required this.className});
 
   @override
-  Widget build(BuildContext context) {    
+  Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(  
-        decoration: BoxDecoration(
+        Container(
+          decoration: BoxDecoration(
             color: Theme.of(context).colorScheme.surfaceVariant,
             borderRadius: BorderRadius.circular(10),
           ),
-          ),
-          ClipRRect(
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10)),
-            child: Container(
-              color: Theme.of(context).colorScheme.onPrimary,
-              height: 345,
-              child:Expanded(
-            flex: 7,
-            child: DataTable2(
-              columnSpacing: 5,
-              fixedTopRows: 1,
-              fixedLeftColumns: 1,
-              dataRowHeight: 30,
-              headingRowHeight: 30,
-              smRatio: 0.3,
-              lmRatio: 1.6,
-              dividerThickness: 0,
-              headingTextStyle: Theme.of(context).textTheme.titleMedium,
-              dataTextStyle: Theme.of(context).textTheme.bodyMedium,
-              dataRowColor: MaterialStateProperty.all<Color>(Colors.transparent),
-              headingRowColor: MaterialStateProperty.all<Color>(Theme.of(context).colorScheme.surfaceVariant),
-              /* decoration: BoxDecoration(
+        ),
+        ClipRRect(
+          borderRadius:
+              const BorderRadius.only(
+                topLeft: Radius.circular(10), 
+                topRight: Radius.circular(10)
+                ),
+          child: Container(
+            color: Theme.of(context).colorScheme.onPrimary,
+            height: MediaQuery.of(context).size.height * 0.370, //345,
+            child: Expanded(
+              flex: 7,
+              child: DataTable2(
+                columnSpacing: 5,
+                fixedTopRows: 1,
+                fixedLeftColumns: 1,
+                dataRowHeight: 30,
+                headingRowHeight: 30,
+                smRatio: 0.3,
+                lmRatio: 1.6,
+                dividerThickness: 0,
+                headingTextStyle: Theme.of(context).textTheme.titleMedium,
+                dataTextStyle: Theme.of(context).textTheme.bodyMedium,
+                dataRowColor: MaterialStateProperty.all<Color>(Colors.transparent),
+                headingRowColor:MaterialStateProperty.all<Color>
+                (Theme.of(context).colorScheme.surfaceVariant),
+                /* decoration: BoxDecoration(
                 borderRadius: BorderR adius.circular(10),
               ), */
-              
-              columns: [
-                const DataColumn2(
-                  label: Text(
-                    '#',
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.center,
-                    maxLines: 1,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold
-                    )
-                  ),
-                  numeric: true,
-                  fixedWidth: 20,
-                  
-                ),
-                DataColumn2(
-                  label: Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Text(
-                      FlutterI18n.translate(context, "tr.order.product"),
-                      textAlign: TextAlign.start,
-                      maxLines: 1,
-                    ),
-                  ),
-                  size: ColumnSize.L,
-                ),
-                DataColumn2(
-                  label: Center(
-                    child: Text(
-                      FlutterI18n.translate(context, "tr.proposal.proposal_note"),
-                    ),
-                  ),  //tedarikci nocu
-                  size: ColumnSize.L,
-                ),
-                DataColumn2(
-                  label: Text(
-                    FlutterI18n.translate(context, "tr.order.amount"),
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.end,
-                  ),
-                  size: ColumnSize.M,
-                  numeric: true,
-                ),
-                DataColumn2(
-                  label: Text(
-                    FlutterI18n.translate(context, "tr.order.price"),
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.end,
-                  ),
+
+                columns: [
+                  const DataColumn2(
+                    label: Text('#',
+                        textDirection: TextDirection.rtl,
+                        textAlign: TextAlign.center,
+                        maxLines: 1,
+                        style: TextStyle(fontWeight: FontWeight.bold)),
                     numeric: true,
-                  size: ColumnSize.M,
-                ),
-                DataColumn2(
-                  label: Text(
-                    FlutterI18n.translate(context, "tr.order.total"),
-                    textDirection: TextDirection.rtl,
-                    textAlign: TextAlign.end, 
+                    fixedWidth: 20,
                   ),
-                  numeric: true,
-                  size: ColumnSize.M,
-                ),
-              ],
-              rows: productsProposalList
-                  .map(
-                    (item) => DataRow2(
-                      cells: [
-                      DataCell(
-                        Text(
-                          (productsProposalList.indexOf(item) + 1).toString(),
-                          textDirection: TextDirection.rtl,
-                          textAlign: TextAlign.center,
-                          maxLines: 1,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            ),
-                        ),
+                  DataColumn2(
+                    label: Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Text(
+                        FlutterI18n.translate(context, "tr.order.product"),
+                        textAlign: TextAlign.start,
+                        maxLines: 1,
                       ),
-                      DataCell(
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
-                          child: Text(
-                            item.productName.toString(),
-                            textDirection: TextDirection.ltr,
-                            textAlign: TextAlign.start,
-                            maxLines: 1,
-                          ),
-                        ),
-                      ), //product_name
-                      DataCell(
-                        Center(
-                          child: Text(
-                            item.proposalNote.toString() == 'null' ? 
-                            '-' : 
-                            item.proposalNote.toString(),
-                            textDirection: TextDirection.ltr,
+                    ),
+                    size: ColumnSize.L,
+                  ),
+                  DataColumn2(
+                    label: Text(
+                      FlutterI18n.translate(context, "tr.proposal.proposal_note"),
+                      textDirection: TextDirection.ltr,
+                      textAlign: TextAlign.start,
+                    ), //tedarikci nocu
+                    size: ColumnSize.L,
+                  ),
+                  DataColumn2(
+                    label: Text(
+                      FlutterI18n.translate(context, "tr.order.amount"),
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.end,
+                    ),
+                    size: ColumnSize.M,
+                    numeric: true,
+                  ),
+                  DataColumn2(
+                    label: Text(
+                      FlutterI18n.translate(context, "tr.order.price"),
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.end,
+                    ),
+                    numeric: true,
+                    size: ColumnSize.M,
+                  ),
+                  DataColumn2(
+                    label: Text(
+                      FlutterI18n.translate(context, "tr.order.total"),
+                      textDirection: TextDirection.rtl,
+                      textAlign: TextAlign.end,
+                    ),
+                    numeric: true,
+                    size: ColumnSize.M,
+                  ),
+                ],
+                rows: productsProposalList
+                    .map(
+                      (item) => DataRow2(cells: [
+                        DataCell(
+                          Text(
+                            (productsProposalList.indexOf(item) + 1).toString(),
+                            textDirection: TextDirection.rtl,
                             textAlign: TextAlign.center,
+                            maxLines: 1,
                             style: const TextStyle(
-                            fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
                             ),
                           ),
-                        )
-                      ), //propsal_note
-                      DataCell(
-                        Text(
-                          '${item.amount} ' 'Adet',//' ${item.productUnit}',
+                        ),
+                        DataCell(
+                          Padding(
+                            padding: const EdgeInsets.only(left: 8.0),
+                            child: Text(
+                              item.productName.toString(),
+                              textDirection: TextDirection.ltr,
+                              textAlign: TextAlign.start,
+                              maxLines: 1,
+                            ),
+                          ),
+                        ), //product_name
+                        DataCell(Text(
+                          item.proposalNote.toString() == 'null'
+                              ? '-'
+                              : item.proposalNote.toString(),
+                          textDirection: TextDirection.ltr,
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 12.0,
+                          ),
+                        )), //propsal_note
+                        DataCell(Text(
+                          '${item.amount} '
+                          'adet', //' ${item.productUnit}',
                           textDirection: TextDirection.ltr,
                           textAlign: TextAlign.end,
                           maxLines: 1,
-                        )
-                      ), //product_unit
-                      DataCell(
-                        Text(
+                        )), //product_unit
+                        DataCell(Text(
                           item.price.toString() == 'null' ? '-' : item.price.toString(),
                           textDirection: TextDirection.rtl,
                           textAlign: TextAlign.end,
                           maxLines: 1,
-                        )
-                      ),
-                      DataCell(
-                        Text(
-                          item.price.toString() == 'null' ? '₺ 20000' 
-                          : calcuteAmount(item.amount.toString(), item.price.toString()),
-                          textDirection: TextDirection.rtl,
-                          textAlign: TextAlign.end,
+                        )),
+                        DataCell(
+                          Text(
+                            item.price.toString() == 'null'
+                                ? '₺ 20000'
+                                : calcuteAmount(item.amount.toString(), item.price.toString()),
+                            textDirection: TextDirection.rtl,
+                            textAlign: TextAlign.end,
+                          ),
                         ),
-                      ),
-                      // DataCell(Text('')),
-                    ]),
-                  )
-                  .toList(),
-            ),
-            ),
+                      ]),
+                    )
+                    .toList(),
+              ),
             ),
           ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          Expanded(
-            flex: 16 ,  
-            child: Container( )
-          ),
-          Expanded(
-            flex: 4,  
-            child: Padding(
-              padding: const EdgeInsets.all(0),
-              child: ProposalSumPanel(
-                className: className,
-                row1: costCalc(productsProposalList,"raw_cost"),
-                row2: costCalc(productsProposalList,"tax_amount"),
-                row3: costCalc(productsProposalList,"total_cost"),/* "$FlutterI18n.translate(context, 'tr.order.proposal')" */ 
-      
-              )
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            Expanded(flex: 16, child: Container()),
+            Expanded(
+              flex: 4,
+              child: Padding(
+                  padding: const EdgeInsets.all(0),
+                  child: ProposalSumPanel(
+                    className: className,
+                    row1: costCalc(productsProposalList, "raw_cost"),
+                    row2: costCalc(productsProposalList, "tax_amount"),
+                    row3: costCalc(productsProposalList, "total_cost"),
+                    /* "$FlutterI18n.translate(context, 'tr.order.proposal')" */
+                  )),
             ),
-          ),
-        ],
-      ),
+          ],
+        ),
       ],
-      
     );
-    
   }
-  
-  
 }
