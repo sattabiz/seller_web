b245c91e025c6f0349529b89af3580b6866f04a2
BilgesuEren <bilgesuueren@gmail.com>
2023-08-31 17:26:45 +0300

    amount has only digits value in create proposal table

--- lib/view/create_proposal_view/create_proposal_view_table.dart
+++ lib/view/create_proposal_view/create_proposal_view_table.dart
@@ -1,5 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../utils/widget_helper.dart';
 import '../../view_model/landing_view_model.dart';
@@ -12,7 +13,8 @@ class FormItem {
   MultipartFile? image;
   String? filename = '  ';
 
-  FormItem({this.category, this.product, this.amount, this.image, this.filename});
+  FormItem(
+      {this.category, this.product, this.amount, this.image, this.filename});
 }
 
 // State Notifier Provider for form items
@@ -34,6 +36,7 @@ class FormItemsNotifier extends StateNotifier<List<FormItem>> {
       state = [...state];
     }
   }
+
   void removeImage(int index) {
     if (index >= 0 && index < state.length) {
       state[index].image = null;
@@ -64,11 +67,15 @@ bool changeIcon(int index, WidgetRef ref) {
   } else {
     return false;
   }
-  
-
 }
+  // bool _validate = false;
+  //  TextEditingController productController = TextEditingController();
+  //  TextEditingController amountController = TextEditingController();
+  //  TextEditingController dropDownController = TextEditingController();
+  //  final _formKeyTable = GlobalKey<FormState>();
 
 class createProposalViewTable extends ConsumerWidget {
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final formItems = ref.watch(formItemProvider);
@@ -86,8 +93,8 @@ class createProposalViewTable extends ConsumerWidget {
                     decoration: BoxDecoration(
                         color: Theme.of(context).colorScheme.surfaceVariant,
                         borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(15),
-                          topRight: Radius.circular(15),
+                          topLeft: Radius.circular(12),
+                          topRight: Radius.circular(12),
                         )),
                     height: MediaQuery.of(context).size.height * 0.05,
                     child: Row(
@@ -168,130 +175,153 @@ class createProposalViewTable extends ConsumerWidget {
       );
     }).toList();
 
-    return Flex(
-      direction: Axis.horizontal,
-      children: [
-        SizedBox(
-          width: 50,
-          child: Padding(
-            padding: const EdgeInsets.all(11.0),
-            child: InkWell(
-              hoverColor: Colors.transparent,
-              child: const Icon(Icons.close),
-              onTap: () {
-                ref.read(formItemProvider.notifier).removeItem(index);
-              },
+    return Form(
+      // key: _formKeyTable,
+      child: Flex(
+        direction: Axis.horizontal,
+        children: [
+          SizedBox(
+            width: 50,
+            child: Padding(
+              padding: const EdgeInsets.all(11.0),
+              child: InkWell(
+                hoverColor: Colors.transparent,
+                child: const Icon(Icons.close),
+                onTap: () {
+                  ref.read(formItemProvider.notifier).removeItem(index);
+                },
+              ),
             ),
           ),
-        ),
-        DropdownMenu<String>(
-          menuHeight: 100,
-          width: screenWidth * 160,
-          inputDecorationTheme: InputDecorationTheme(
-            filled: true,
-            fillColor: Theme.of(context).colorScheme.onPrimary,
-            constraints: const BoxConstraints(maxHeight: 40),
-            contentPadding: const EdgeInsets.only(left: 10.0),
-            floatingLabelAlignment: FloatingLabelAlignment.start,
-            border: const OutlineInputBorder(),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(
-                  color: Theme.of(context).colorScheme.onSurfaceVariant),
-            ),
-          ),
-          label: Text(
-            'Kategori',
-            style: Theme.of(context).textTheme.bodySmall,
-            textAlign: TextAlign.start,
-          ),
-          menuStyle: MenuStyle(
-            fixedSize: MaterialStateProperty.all(Size(screenWidth * 160, 100)),
-            alignment: AlignmentGeometry.lerp(
-              Alignment.bottomLeft,
-              Alignment.bottomLeft,
-              0.5,
-            ),
-          ),
-          dropdownMenuEntries: dropDownMenuCategory,
-          onSelected: (value) {
-            formItem.category = value;
-          },
-        ),
-        const Spacer(
-          flex: 1,
-        ),
-        Expanded(
-          flex: 10,
-          child: TextFormField(
-            cursorColor: Theme.of(context).colorScheme.onBackground,
-            enableInteractiveSelection: false,
-            decoration: InputDecoration(
+          DropdownMenu<String>(
+            menuHeight: 100,
+            width: screenWidth * 160,
+            inputDecorationTheme: InputDecorationTheme(
               filled: true,
               fillColor: Theme.of(context).colorScheme.onPrimary,
-              border: const OutlineInputBorder(),
-              contentPadding: const EdgeInsets.only(
-                  left: 10.0, top: 0.0, bottom: 0.0, right: 0.0),
               constraints: const BoxConstraints(maxHeight: 40),
-              labelText: 'Ürün',
+              contentPadding: const EdgeInsets.only(left: 10.0),
+              floatingLabelAlignment: FloatingLabelAlignment.start,
+              
+              border: const OutlineInputBorder(),
               focusedBorder: OutlineInputBorder(
                 borderSide: BorderSide(
                     color: Theme.of(context).colorScheme.onSurfaceVariant),
               ),
-              labelStyle: Theme.of(context).textTheme.bodySmall,
             ),
-            onChanged: (value) {
-              formItem.product = value;
+            label: Text(
+              'Kategori',
+              style: Theme.of(context).textTheme.bodySmall,
+              textAlign: TextAlign.start,
+            ),
+            menuStyle: MenuStyle(
+              fixedSize: MaterialStateProperty.all(Size(screenWidth * 160, 100)),
+              alignment: AlignmentGeometry.lerp(
+                Alignment.bottomLeft,
+                Alignment.bottomLeft,
+                0.5,
+              ),
+            ),
+            dropdownMenuEntries: dropDownMenuCategory,
+            onSelected: (value) {
+              formItem.category = value;
             },
           ),
-        ),
-        const Spacer(
-          flex: 1,
-        ),
-        Expanded(
-          flex: 4,
-          child: Container(
-            width: 120,
-            // constraints: BoxConstraints(maxWidth: screenWidth * 120, minWidth: screenWidth * 40),
+          const Spacer(
+            flex: 1,
+          ),
+          Expanded(
+            flex: 10,
             child: TextFormField(
               cursorColor: Theme.of(context).colorScheme.onBackground,
+              enableInteractiveSelection: false,
               decoration: InputDecoration(
-                constraints: const BoxConstraints(maxHeight: 40),
                 filled: true,
                 fillColor: Theme.of(context).colorScheme.onPrimary,
+                // errorText: _validate ? 'Value Cant Be Empty' : null,
                 border: const OutlineInputBorder(),
                 contentPadding: const EdgeInsets.only(
-                    left: 10.0, top: 0.0, bottom: 0.0, right: 5.0),
-                labelText: 'Miktar',
-                labelStyle: Theme.of(context).textTheme.bodySmall,
+                    left: 10.0, top: 0.0, bottom: 0.0, right: 0.0),
+                constraints: const BoxConstraints(maxHeight: 40),
+                labelText: 'Ürün',
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(
                       color: Theme.of(context).colorScheme.onSurfaceVariant),
                 ),
-                suffixText: 'adet',
+                labelStyle: Theme.of(context).textTheme.bodySmall,
               ),
               onChanged: (value) {
-                formItem.amount = value;
+                formItem.product = value;
+              },
+              // validator: (String? value) {
+              //   if (value == null || value.isEmpty) {
+              //     return 'Lütfen ürün giriniz.';
+              //   }
+              //   return null;
+              // },
+            ),
+          ),
+          const Spacer(
+            flex: 1,
+          ),
+          Expanded(
+            flex: 4,
+            child: Container(
+              width: 120,
+              // constraints: BoxConstraints(maxWidth: screenWidth * 120, minWidth: screenWidth * 40),
+              child: TextFormField(
+                autocorrect: true,
+                inputFormatters: [
+                  FilteringTextInputFormatter.digitsOnly,
+                ],
+                cursorColor: Theme.of(context).colorScheme.onBackground,
+                decoration: InputDecoration(
+                  constraints: const BoxConstraints(maxHeight: 40),
+                  filled: true,
+                  fillColor: Theme.of(context).colorScheme.onPrimary,
+                  border: const OutlineInputBorder(),
+                  contentPadding: const EdgeInsets.only(
+                      left: 10.0, top: 0.0, bottom: 0.0, right: 5.0),
+                  labelText: 'Miktar',
+                  labelStyle: Theme.of(context).textTheme.bodySmall,
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  ),
+                  suffixText: 'adet',
+                ),
+                onChanged: (value) {
+                  formItem.amount = value;
+                },
+                // validator: (String? value) {
+                //   if (value == "" || value!.isEmpty) {
+                //     print("aaaaaaaaaaaa");
+                //     return 'Lütfen miktar giriniz.';
+                //   }
+                //   return null;
+                // },
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 50,
+            child: IconButton(
+              icon: Icon(changeIcon(index, ref)
+                  ? Icons.image_outlined
+                  : Icons.attach_file),
+              iconSize: 25,
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return CreateProposalShowDialog(itemIndex: index);
+                  },
+                );
               },
             ),
           ),
-        ),
-        SizedBox(
-          width: 50,
-          child: IconButton(
-            icon: Icon(
-                changeIcon(index, ref) ? Icons.image_outlined : Icons.attach_file),
-            iconSize: 25,
-            onPressed: () {
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return CreateProposalShowDialog(itemIndex: index);
-                },
-              );
-            },
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
